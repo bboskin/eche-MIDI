149f437ab88161c23b072565b438136f4260beb8
Ben Boskin <b.boskin96@gmail.com>
2021-02-03 19:56:12 -0500

    encode and decode of csv

---
+++ midi-csv/translate.rkt
@@ -0,0 +1,120 @@
+#lang racket
+
+(require csv-reading csv-writing)
+
+
+;; reads CSV file into Racket data
+(define (read-MIDI filename)
+  (define reader
+    (make-csv-reader-maker
+     '((separator-chars            #\,)
+       (strip-leading-whitespace?  . #t)
+       (strip-trailing-whitespace? . #t))))
+  (let ((next-row (reader (open-input-file filename))))
+    (filter
+     (λ (x) x)
+     (csv-map
+      (λ (x)
+        (match x
+          [`(,_ ,time "Header" . ,c) #f]
+          [`(,_ ,time "Start_track" . ,c) #f]
+          [`(,_ ,time "Title_t" . ,c) #f]
+          [`(,_ ,time "Time_signature" . ,c) #f]
+          [`(,_ ,time "End_track" . ,c) #f]
+          [`(,_ ,time "End_of_file" . ,c) #f]
+          ;;;;;;;
+          [`(,_ ,time "Note_on_c" ,chan ,pitch ,vel)
+           (list (string->number time) "on" (string->number pitch))]
+          [`(,_ ,time "Note_off_c" ,chan ,pitch ,vel)
+           (list (string->number time) "off" (string->number pitch))]))
+      next-row))))
+
+(define (fix-state s)
+  (match s
+    ["on" "Note_on_c"]
+    ["off" "Note_off_c"]))
+
+(define DEFAULT-TRASH 0)
+(define DEFAULT-CHANNEL 1)
+(define DEFAULT-VELOCITY 60)
+
+(define (write-MIDI csv)
+  (map
+   (λ (x)
+     (match x
+       [`(,time ,state ,pitch)
+        (list DEFAULT-TRASH
+              (number->string time)
+              (fix-state state)
+              DEFAULT-CHANNEL
+              (number->string pitch)
+              DEFAULT-VELOCITY)]))
+   csv))
+
+
+(define EXAMPLE "unquantized/unquantized.csv")
+
+(define (set-cons x ls) (if (member x ls) ls (cons x ls)))
+(define (to-set ls) (foldr set-cons '() ls))
+(define (set-difference s1 s2)
+  (foldr
+   (λ (x a) (if (member x s2) a (cons x a)))
+   '()
+   s1))
+
+(define (add-to-count pitches time counts)
+  (map
+   (λ (x)
+     (if (= (car x) time)
+         (cons (car x) (sort (to-set (append (cdr x) pitches)) <))
+         x))
+   counts))
+
+(define (remove-from-count pitch time counts)
+  (map
+   (λ (x)
+     (if (= (car x) time)
+         (cons (car x) (remove pitch (cdr x)))
+         x))
+   counts))
+
+;; translates Racket-data CSV into text version
+(define (csv->words csv)
+  (let ((MAX (foldr max 0 (map first csv))))
+    (let loop ((track csv)
+               (on '())
+               (i 0)
+               (counts (build-list (add1 MAX) (λ (x) `(,x)))))
+    (match track
+      ['() (map
+            (λ (x)
+              (let ((word (list->string (map integer->char (cdr x)))))
+                (list (car x) word)))
+            counts)]
+      [`((,time ,state ,pitch) . ,d)
+       (if (<= i time)
+           (loop track on (add1 i) (add-to-count on i counts))
+           (match state
+             ["on" (let ((on (cons pitch on)))
+                     (loop d on time (add-to-count on time counts)))]
+             ["off"
+              (let ((on (remove pitch on)))
+                (loop d on time (remove-from-count pitch time counts)))]))]
+      [else (error 'parse-csv (format "unknown instr ~s" track))]))))
+
+(define ((make-note-on time) n) `(,time "on" ,n))
+(define ((make-note-off time) n) `(,time "off" ,n))
+
+(define (words->csv words)
+  (let loop ((track words)
+             (on '()))
+    (match track
+      ['() '()]
+      [`((,k ,word) . ,d)
+       (let ((word (string->list word)))
+         (let ((turned-on (set-difference word on))
+               (turned-off (set-difference on word)))
+           (append
+            (map (make-note-on k) (map char->integer turned-on))
+            (map (make-note-off k) (map char->integer turned-off))
+            (loop d (to-set (append word (set-difference on turned-off)))))))])))
